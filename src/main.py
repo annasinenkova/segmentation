84994d21913553038a295cd63a200e7e98173033
Sinenkova Anna <sinenkovaj@gmail.com>
2024-04-25 19:25:13 +0300

    Image upload

---
+++ src/main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+
+
+def set_screen_shape(root, width, height):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    total_width = (screen_width // 2) - (width // 2)
+    total_height = (screen_height // 2) - (height // 2)
+    root.geometry(f'{width}x{height}+{total_width}+{total_height}')
+
+
+def image_upload():
+    path = filedialog.askopenfilename(filetypes=[('Image File','.jpg .png .jpeg')])
+    
+    if not len(path):
+        return
+    
+    img = Image.open(path)
+    upload_bttn.destroy()
+    
+    raw_img = ImageTk.PhotoImage(img)
+    img_show = Label(root, image=raw_img)
+    img_show.image = raw_img
+  
+    set_screen_shape(root, img.width, img.height)
+
+    img_show.pack(side='bottom', fill='both', expand='yes')
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Segmentation')
+    
+    set_screen_shape(root, 500, 300)
+
+    upload_bttn = Button(root, text='Choose image', command=image_upload)
+    upload_bttn.pack(side='bottom', pady=50)
+    
+    root.mainloop()
+

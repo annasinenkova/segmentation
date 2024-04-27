80059023e3410d9205ab57b996489ca2a030ebfc
Sinenkova Anna <sinenkovaj@gmail.com>
2024-04-27 13:50:24 +0300

    Docstrings

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from tkinter import *
+"""The main file that runs the application."""
+
+import tkinter as tk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 
@@ -6,6 +8,7 @@ from segmentation import segmentation
 
 
 def set_screen_shape(root, width, height):
+    """Set the screen size."""
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     total_width = (screen_width // 2) - (width // 2)
@@ -14,38 +17,40 @@ def set_screen_shape(root, width, height):
 
 
 def image_upload():
-    path = filedialog.askopenfilename(filetypes=[('Image File','.jpg .png .jpeg')])
-    
+    """Load an image and display a segmented image on the screen."""
+    path = filedialog.askopenfilename(
+           filetypes=[('Image File', '.jpg .png .jpeg')]
+    )
+
     if not len(path):
         return
-    
+
     img = Image.open(path)
     upload_bttn.destroy()
-    
+
     raw_img = ImageTk.PhotoImage(img)
-    img_show = Label(root, image=raw_img)
+    img_show = tk.Label(root, image=raw_img)
     img_show.image = raw_img
-  
+
     set_screen_shape(root, img.width, img.height)
     img_show.pack(side='bottom', fill='both', expand='yes')
-    
+
     segmented_img = segmentation(path)
-    
+
     segmented_img = ImageTk.PhotoImage(segmented_img)
-    segmented_img_show = Label(root, image=segmented_img)
+    segmented_img_show = tk.Label(root, image=segmented_img)
     segmented_img_show.image = segmented_img
     img_show.destroy()
     segmented_img_show.pack(side='bottom', fill='both', expand='yes')
 
 
 if __name__ == '__main__':
-    root = Tk()
+    root = tk.Tk()
     root.title('Segmentation')
-    
+
     set_screen_shape(root, 500, 300)
 
-    upload_bttn = Button(root, text='Choose image', command=image_upload)
+    upload_bttn = tk.Button(root, text='Choose image', command=image_upload)
     upload_bttn.pack(side='bottom', pady=50)
-    
-    root.mainloop()
 
+    root.mainloop()
--- src/segmentation.py
+++ src/segmentation.py
@@ -1,15 +1,16 @@
+"""Image segmentation."""
+
 from PIL import Image
 import torchvision
-
 import warnings
 import torch
-
 import random
 import numpy as np
 import cv2
 
 
 def filter(output):
+    """Return objects with a probability >= 0.5."""
     boxes = []
     labels = []
     scores = []
@@ -19,52 +20,59 @@ def filter(output):
             boxes.append(output['boxes'][i].int().cpu().numpy())
             labels.append(output['labels'][i].cpu().numpy())
             scores.append(output['scores'][i].cpu().numpy())
-        
+
     return boxes, labels, scores
-    
-    
+
+
 def categories_from_txt(path):
+    """Read categories from a file and return array."""
     categories = []
     with open(path, 'r') as file:
         for line in file:
             categories.append(line.strip())
 
     return categories
-    
-    
+
+
 def segmented_image(img, boxes, labels, scores):
+    """Return a segmented image."""
     categories = categories_from_txt('../data/categories.txt')
-        
+
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in categories]
-        
+
     result_image = np.array(img)
-        
+
     for box, label, score, in zip(boxes, labels, scores):
         color = random.choice(colors)
         tl = round(0.002 * max(result_image.shape[0:2])) + 1
         c1, c2 = (box[0], box[1]), (box[2], box[3])
         cv2.rectangle(result_image, c1, c2, color, thickness=tl)
-        display_txt = "%s: %.1f%%" % (categories[label], 100*score)
+        text = "%s: %.1f%%" % (categories[label], 100*score)
         tf = max(tl - 1, 1)
-        t_size = cv2.getTextSize(display_txt, 0, fontScale=tl / 3, thickness=tf)[0]
+        t_size = cv2.getTextSize(text, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(result_image, c1, c2, color, -1)
-        cv2.putText(result_image, display_txt, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-    
+        cv2.putText(result_image, text,
+                    (c1[0], c1[1] - 2), 0, tl / 3,
+                    [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
     return Image.fromarray(result_image.astype(np.uint8))
 
 
 def segmentation(path):
+    """Segment an image."""
     img = Image.open(path)
-    
+
     torch.set_grad_enabled(False)
     warnings.filterwarnings("ignore")
-    pretrained_model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+    pretrained_model = torchvision.models.detection.maskrcnn_resnet50_fpn(
+                       pretrained=True
+    )
     pretrained_model = pretrained_model.eval().cpu()
 
     img_tensor = torchvision.transforms.functional.to_tensor(img).cpu()
     output = pretrained_model([img_tensor])[0]
-    
+
     boxes, labels, scores = filter(output)
 
     return segmented_image(img, boxes, labels, scores)

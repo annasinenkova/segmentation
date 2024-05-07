c6008dec269b3666e88fca9dc76a39c2e78b5c11
Sinenkova Anna <sinenkovaj@gmail.com>
2024-05-08 02:27:02 +0300

    Doit

---
+++ dodo.py
@@ -0,0 +1,32 @@
+"""Doit tasks."""
+
+
+def task_flake8():
+    """Run flake8."""
+    return {
+        'actions': ['flake8'],
+        'verbosity': 2
+    }
+
+
+def task_pydocstyle():
+    """Run pydocstyle."""
+    return {
+        'actions': ['pydocstyle'],
+        'verbosity': 2
+    }
+
+
+def task_tests():
+    """Run tests."""
+    return {
+        'actions': ['pytest'],
+        'verbosity': 2
+    }
+
+
+def task_html():
+    """Build html."""
+    return {
+        'actions': ['make -C docs html']
+    }
